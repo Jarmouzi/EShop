4558f8a27e6129fc572f0946c67cd865738fb18d
MGHMirsaeid <m.mirsaeid@gmail.com>
2024-05-24 16:02:23 +0330

    check id is zero added to all indexes ErrorLog added Banner CRUD fixed

---
+++ EShop.Log/Model/ErrorLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.LogService.Model
+{
+    public class ErrorLog
+    {
+        public ErrorLog()
+        {
+            Date = DateTime.Now;
+        }
+        public Int64 Id { get; set; }
+        public string Action { get; set; }
+        public string Error { get; set; }
+        public string Username { get; set; }
+        public string Parameters { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
